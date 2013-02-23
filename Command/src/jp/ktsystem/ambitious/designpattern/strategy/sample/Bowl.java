009820ce59efab3bf3a1c4cbb97399a96067b297
Ryoichi Obara <kts.ryoichi.obara+github@gmail.com>
2013-02-23 13:24:56 +0900

    Add Command pattern

--- Command/src/jp/ktsystem/ambitious/designpattern/strategy/sample/Bowl.java
+++ Command/src/jp/ktsystem/ambitious/designpattern/strategy/sample/Bowl.java
@@ -6,15 +6,20 @@ import java.util.List;
 
 public class Bowl implements Serializable {
 
+	private static final long serialVersionUID = 6651030530734488578L;
+
 	private IfNoodle noodle;
 	private IfSoup soup;
 	private List<Topping> topping;
+
+	private boolean complete = false;
 
 	public Bowl() {
 		this.topping = new ArrayList<Topping>();
 	}
 
 	public Bowl putNoodle(IfNoodle noodle) {
+		if (complete) throw new WrongOperationException("complete");
 		if (null != this.noodle)     throw new WrongOperationException(this.noodle);
 		if (null != this.soup)       throw new WrongOperationException(this.soup);
 		if (!this.topping.isEmpty()) throw new WrongOperationException(this.topping);
@@ -23,6 +28,7 @@ public class Bowl implements Serializable {
 	}
 
 	public Bowl putSoup(IfSoup soup) {
+		if (complete) throw new WrongOperationException("complete");
 		if (null == this.noodle)     throw new WrongOperationException(this.noodle);
 		if (null != this.soup)       throw new WrongOperationException(this.soup);
 		if (!this.topping.isEmpty()) throw new WrongOperationException(this.topping);
@@ -31,14 +37,30 @@ public class Bowl implements Serializable {
 	}
 
 	public Bowl addTopping(Topping topping) {
+		if (complete) throw new WrongOperationException("complete");
 		if (null == this.noodle)     throw new WrongOperationException(this.noodle);
 		if (null == this.soup)       throw new WrongOperationException(this.soup);
 		this.topping.add(topping);
 		return this;
 	}
 
-	public void build() {
+	public String eat() {
 		//
+		this.complete = true;
+
+		StringBuilder s = new StringBuilder();
+
+		s.append(noodle.getName()).append(" in ").append(soup.getName());
+
+		if (!topping.isEmpty()) s.append(" with ");
+		for (Topping t : topping) {
+			s.append(t.getName()).append(", ");
+		}
+		if (!topping.isEmpty()) s.setLength(s.length() - ", ".length());
+
+		s.append(".");
+
+		return s.toString();
 	}
 
 }
--- Command/src/jp/ktsystem/ambitious/designpattern/strategy/sample/Main.java
+++ Command/src/jp/ktsystem/ambitious/designpattern/strategy/sample/Main.java
@@ -1,10 +1,14 @@
 package jp.ktsystem.ambitious.designpattern.strategy.sample;
 
+import jp.ktsystem.ambitious.designpattern.strategy.sample.CommandClass.Order;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		String order = "";
 
+		Bowl bowl = CommandClass.createBowlWithNoodle(Order.TANUKI_SOBA);
+
+		System.out.println(bowl.eat());
 	}
+
 }
